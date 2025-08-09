c7d80d899b8444cc915443322a5a1a2d97aaecdd
zanzalaachal21@gmail.com <ajinkyabhise1226@gmail.com>
2025-08-09 19:15:10 +0530

    Add .gitignore

---
+++ routes/newsRoutes.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const protect = require('../middleware/auth');
+const fetchNews = require('../utils/fetchNews');
+
+const router = express.Router();
+
+// Get news based on user's preference
+router.get('/', protect, async (req, res) => {
+    try {
+        const categories = req?.user?.preferences?.categories || ['general'];
+        const allNews = [];
+
+        console.log(categories,"categories")
+
+
+        for (const category of categories) {
+            const news = await fetchNews(category);
+            allNews.push(...news);
+        }
+
+        res.json(allNews);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+module.exports = router;
